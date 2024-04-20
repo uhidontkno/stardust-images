25939256451ed3724d58dd834ff33efc6eec720e
incognitotgt <tgt@incognitotgt.me>
2024-04-20 00:17:50 -0400

    alhamdulliah this works

--- debian/start.sh
+++ debian/start.sh
@@ -3,5 +3,11 @@ vncserver -kill :1
 sudo rm -rf /run/dbus
 sudo mkdir -p /run/dbus
 sleep 1
-vncserver :1 -passwd /home/stardust/.vnc/passwd &
+echo "while :
+do
+vncserver :1 -passwd /home/stardust/.vnc/passwd -localhost no
+sleep 5
+done
+" | bash &
+sleep 1
 tail -f /dev/null
